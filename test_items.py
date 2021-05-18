c5160a68ec37b8a6bf6c7ea58dce4f82261837e6
smolnikovasa <smolnikova_sa@mail.ru>
2021-05-18 15:19:06 +0700

    Тестирование: проверка наличия кнопки на странице

--- test_items.py
+++ test_items.py
@@ -1,1 +1,16 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_chek_button(browser):
+    try:
+        browser.get(link)
+        time.sleep(30)
+        browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
+        k=1
+    except NoSuchElementException:
+        k=0
+    finally:
+        assert k==1, "Кнопка не найдена"
+
 
